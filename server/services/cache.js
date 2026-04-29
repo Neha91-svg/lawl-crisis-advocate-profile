77d9816c34ad092da89a65d9aef89b5f00ce6c74
Neha91-svg <ng941628@gmail.com>
2026-04-29 08:25:35 +0530

    Implement in-memory caching with source-specific TTL for News and Location APIs

--- server/services/cache.js
+++ server/services/cache.js
@@ -2,7 +2,6 @@ const NodeCache = require('node-cache');
 
 // Initialize cache
 // stdTTL: 0 means keys never expire by default (we will set them per key)
-// checkperiod: period in seconds to check for expired keys
 const apiCache = new NodeCache({ stdTTL: 0, checkperiod: 60 });
 
 const CACHE_KEYS = {
